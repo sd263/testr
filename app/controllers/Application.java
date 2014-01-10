21f2e0b63df33dafbb40fb60e0a8a26b5fe7c93c
sd263 <simondate@yahoo.co.uk>
2014-01-10 12:52:02 +0000

    Attempted to put table showing all tests made. Still doesn't compile yet.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,18 +2,34 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
+import models.*;
 
 public class Application extends Controller {
+	
+	
+    public static Result GO_HOME = redirect(
+            routes.Application.list(0, "name", "asc", "")
+        );
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+    	return GO_HOME;
     }
     
     public static Result createTest() {
 		return TODO;
     	
+    }
+    
+    
+    
+    public static Result list(int page, String sortBy, String order, String filter) {
+        return ok(
+            list.render(
+                Test.page(page, 10, sortBy, order, filter),
+                sortBy, order, filter
+            )
+        );
     }
 
 }
--- app/models/Test.java
+++ app/models/Test.java
@@ -1,21 +1,43 @@
 package models;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.*;
+import com.avaje.ebean.Page;
 
+import play.data.validation.Constraints;
 import play.db.ebean.Model;
+import play.db.ebean.Model.Finder;
 
+/**
+ * Test entity managed by Ebean
+ */
 @Entity
 public class Test extends Model{
 	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	public long testID;
+	public Long testID;
 	
+	@Constraints.Required
 	public String testName;
 	
+
+	@Constraints.Required
 	public String testDesc;
+	
+    public static Finder<Long,Test> find = new Finder<Long,Test>(Long.class, Test.class); 
+	
+    public static Page<Test> page(int page, int pageSize, String sortBy, String order, String filter) {
+        return 
+            find.where()
+                .ilike("name", "%" + filter + "%")
+                .orderBy(sortBy + " " + order)
+                .fetch("Test")
+                .findPagingList(pageSize)
+                .setFetchAhead(false)
+                .getPage(page);
+    }
 
 
 }
